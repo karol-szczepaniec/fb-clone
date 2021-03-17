97c3aac2576b7d9f77883c8faef6d0033488d810
charlie_sz <karol.szczepaniec@gopos.pl>
2021-03-17 22:50:31 +0100

    add widget section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css"
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
-import Post from "./components/Post";
+import Widgets from "./components/Widgets";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <div className={"app__body"}>
             <Sidebar/>
             <Feed/>
+            <Widgets/>
         </div>
     </div>
   );
