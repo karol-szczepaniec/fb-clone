e2078de33a44a3918e2c5471581e2c0db5f1c0da
charlie_sz <karol.szczepaniec@gopos.pl>
2021-03-17 15:25:52 +0100

    add: - material ui core - material ui icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,13 @@ import "../style/Header.css"
 export default function Header(){
     return(
         <div className={"header"}>
-
+            <div className={"header__left"}>
+                <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"}
+                     alt={"fb-logo"}
+                />
+            </div>
+            <div className={"header__middle"}></div>
+            <div className={"header__right"}></div>
         </div>
     )
 }
