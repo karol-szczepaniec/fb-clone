823313b30a6058ab9f741ad45f905ad2cda07170
charlie_sz <karol.szczepaniec@gopos.pl>
2021-03-17 17:32:58 +0100

    add header with BEM styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,16 @@
 import React from "react";
 import "../style/Header.css"
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import AddIcon from '@material-ui/icons/Add'
+import ForumIcon from '@material-ui/icons/Forum'
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import {Avatar, IconButton} from "@material-ui/core";
 
 export default function Header(){
     return(
@@ -8,9 +19,47 @@ export default function Header(){
                 <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"}
                      alt={"fb-logo"}
                 />
+                <div className={"header__input"}>
+                    <SearchIcon/>
+                    <input placeholder={"Search Facebook"} type={"text"}/>
+                </div>
             </div>
-            <div className={"header__middle"}></div>
-            <div className={"header__right"}></div>
+            <div className={"header__center"}>
+                <div className={"header__option header__option--active"}>
+                    <HomeIcon fontSize={"large"}/>
+                </div>
+                <div className={"header__option"}>
+                    <FlagIcon fontSize={"large"}/>
+                </div>
+                <div className={"header__option"}>
+                    <SubscriptionsOutlinedIcon fontSize={"large"}/>
+                </div>
+                <div className={"header__option"}>
+                    <StorefrontOutlinedIcon fontSize={"large"}/>
+                </div>
+                <div className={"header__option"}>
+                    <SupervisedUserCircleIcon fontSize={"large"}/>
+                </div>
+            </div>
+            <div className={"header__right"}>
+                <div className={"header__info"}>
+                    <Avatar/>
+                    <h4>Karol Szczepaniec</h4>
+                </div>
+
+                <IconButton>
+                    <AddIcon/>
+                </IconButton>
+                <IconButton>
+                    <ForumIcon/>
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon/>
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon/>
+                </IconButton>
+            </div>
         </div>
     )
 }
