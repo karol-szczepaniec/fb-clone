185da9a1dfb2de79c50a0cdd3350baa51e9080bc
charlie_sz <karol.szczepaniec@gopos.pl>
2021-03-17 15:02:11 +0100

    create service on firebase and add credentials

---
+++ src/services/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-6XaUy_pOA2tp-cXZXkFLyqERMQPWcUA",
+    authDomain: "facebook-clone-21dad.firebaseapp.com",
+    projectId: "facebook-clone-21dad",
+    storageBucket: "facebook-clone-21dad.appspot.com",
+    messagingSenderId: "585146175587",
+    appId: "1:585146175587:web:48176944705222bb83a856"
+};
